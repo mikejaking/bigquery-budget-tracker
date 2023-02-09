525a96f70f26511aae5b8075aec859f1f3a7b7b1
daithi-walker <daithi.walker@gmail.com>
2023-02-09 08:34:59 -0800

    formatting and added descriptions

--- budgets_vs_actual.sql
+++ budgets_vs_actual.sql
@@ -4,6 +4,10 @@ Dave Walker
 bigquery-budget-tracker/budgets_vs_actual.sql
 Version 1
 Google Standard SQL
+
+Description: This view joins the above views for budget and spend and generates a dataset that contains additional
+information such as a daily running total for spend as well as a dynamic budget for each day that is based on the
+amount of underspend/overspend in relation to the current budget.
 */
 
 WITH
@@ -33,31 +37,34 @@ WITH
 SELECT
   date,
   program,
-	(actual_monthly_running_total - actual_daily) AS actual_rt_sod,
-  actual_daily as actual,
-  actual_monthly_running_total as actual_rt_eod,
+  (actual_monthly_running_total - actual_daily) AS actual_rt_sod,
+  actual_daily AS actual,
+  actual_monthly_running_total AS actual_rt_eod,
   GREATEST(0, ROUND((budget_monthly - (actual_monthly_running_total - actual_daily)) / (EXTRACT(DAY
+        FROM
+          LAST_DAY(date, MONTH)) - EXTRACT(DAY
+        FROM
+          CASE
+            WHEN date <= CURRENT_DATE() THEN date
+          ELSE
+          CURRENT_DATE
+        END
+          ) + 1),2)) AS budget_dynamic,
+  CASE
+    WHEN date > CURRENT_DATE() THEN 0
+  ELSE
+  ROUND((budget_monthly - (actual_monthly_running_total - actual_daily)) / (EXTRACT(DAY
       FROM
         LAST_DAY(date, MONTH)) - EXTRACT(DAY
       FROM
-        CASE WHEN date <= CURRENT_DATE() THEN date ELSE CURRENT_DATE END) + 1),2)) AS budget_dynamic,
-  CASE WHEN date > CURRENT_DATE() THEN 0 ELSE ROUND((budget_monthly - (actual_monthly_running_total - actual_daily)) / (EXTRACT(DAY
-      FROM
-        LAST_DAY(date, MONTH)) - EXTRACT(DAY
-      FROM
-        date) + 1) - actual_daily,2) END AS underspend_dynamic,
+        date) + 1) - actual_daily,2)
+END
+  AS underspend_dynamic,
   ROUND(budget_daily,2) AS budget_crude,
-	ROUND(budget_daily	- actual_daily,2) AS underspend_crude,
-	budget_monthly as target_budget_monthly,
-      --  round((budget_monthly - actual_monthly_running_total),2) as eod_remaining_budget_monthly,
-      --  EXTRACT(DAY FROM LAST_DAY(date, MONTH)) - EXTRACT(DAY from date) + 1 as remaining_days_in_month
+  ROUND(budget_daily - actual_daily,2) AS underspend_crude,
+  budget_monthly AS target_budget_monthly
 FROM
   main
-WHERE
-  1=1
-	-- and date between '2023-01-01' and '2023-02-28'
-	-- and program = 'Brand'
-	-- and program = 'Campaign Name 6'
 ORDER BY
   date,
   program
